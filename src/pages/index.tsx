463574d11afc19a8d698261f680b218167d99191
Morgana <morganabagatini@gmail.com>
2022-05-02 09:08:17 -0300

    home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import { Header } from "../components/Header";
+import styles from "../styles/Home.module.scss";
+
+import imgBannerMobile from "../../public/images/mobile/image-header.jpg";
+import imgBannerDesktop from "../../public/images/desktop/image-header.jpg";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.banner}>
+        <Image src={imgBannerDesktop} alt="banner" layout="responsive" />
+        <h1>WE ARE CREATIVES</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
