bc10cb4625a1e276745848f087ce19a77143f8de
Morgana <morganabagatini@gmail.com>
2022-05-11 11:32:41 -0300

    menu responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import imgMenu from "/public/images/icon-hamburger.svg";
 
-import styles from "./Header.module.scss";
+import { useBreakpoint } from "../../hooks/useBreakpoint";
 import { MenuItem } from "./MenuItem";
 
+import styles from "./Header.module.scss";
+import { useState } from "react";
+
 export function Header() {
+  const point = useBreakpoint();
+
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const showMenu = (point == "xs" && menuIsOpen) || point != "xs";
+
+  let menuClasses = [styles.menu];
+  if (showMenu) menuClasses.push(styles["show-menu"]);
+  if (point == "xs") menuClasses.push(styles["menu-mobile"]);
+
   return (
     <div className={styles.container}>
       <Image
@@ -14,7 +27,16 @@ export function Header() {
         layout="fixed"
       />
 
-      <menu className={styles.menu}>
+      {point == "xs" && (
+        <button
+          className={styles["menu-icon"]}
+          onClick={() => setMenuIsOpen(!menuIsOpen)}
+        >
+          <Image src={imgMenu} alt="menu" />
+        </button>
+      )}
+
+      <menu className={menuClasses.join(" ")}>
         <MenuItem href="/">About</MenuItem>
         <MenuItem href="/">Services</MenuItem>
         <MenuItem href="/">Projects</MenuItem>
