ab18f7f8961a533c95a07dbceda59b937ae15d66
Morgana <morganabagatini@gmail.com>
2022-05-10 22:59:01 -0300

    index mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,33 @@
 import Image from "next/image";
 
 import { Header } from "../components/Header";
+import { Grid } from "../components/Grid";
 import styles from "../styles/Home.module.scss";
 
-import imgBannerMobile from "../../public/images/mobile/image-header.jpg";
-import imgBannerDesktop from "../../public/images/desktop/image-header.jpg";
+import imgBannerMobile from "/public/images/mobile/image-header.jpg";
+import imgBannerDesktop from "/public/images/desktop/image-header.jpg";
+import imgArrow from "/public/images/icon-arrow-down.svg";
+import { useBreakpoint } from "../hooks/useBreakpoint";
 
 function Home() {
+  const point = useBreakpoint();
+
+  const imgBanner = point == "xs" ? imgBannerMobile : imgBannerDesktop;
+
   return (
     <div>
       <Header />
 
-      <div className={styles.banner}>
-        <Image src={imgBannerDesktop} alt="banner" layout="responsive" />
+      <section className={styles.banner}>
+        <Image src={imgBanner} alt="banner" layout="responsive" />
         <h1>WE ARE CREATIVES</h1>
-        <img
-          src="/images/icon-arrow-down.svg"
-          alt="arrow"
-          className={styles.icon}
-        />
-      </div>
+
+        <div className={styles["icon-container"]}>
+          <Image src={imgArrow} alt="Arrow icon" />
+        </div>
+      </section>
+
+      <Grid />
     </div>
   );
 }
